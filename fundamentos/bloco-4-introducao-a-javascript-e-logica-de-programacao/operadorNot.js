42f9eff755e638a6db4237ded2af3be103176ea5
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-03-07 20:38:58 -0300

    exercicio operadorNot.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/operadorNot.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/operadorNot.js
@@ -1,2 +1,10 @@
 console.log((2 + 2) === 4);
 console.log(!(2 + 2) === 4);
+console.log(!42); // false
+
+console.log(!0); // true
+// O número 0 tem o valor "falsy" no javascript. 
+// Logo, seu oposto é true.
+console.log(!null); // true
+console.log(!undefined); // true
+console.log(!undefined); // true
