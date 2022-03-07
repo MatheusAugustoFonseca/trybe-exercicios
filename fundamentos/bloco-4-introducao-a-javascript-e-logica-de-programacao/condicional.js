a03a68bc093ca800a9a1114c3446f54a603e4500
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-03-07 19:03:36 -0300

    switch case

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/condicional.js
@@ -0,0 +1,15 @@
+let resultado ="reprovada";
+switch (resultado){
+    case "aprovada":
+    console.log("aprovada");
+    break
+    case "lista":
+        console.log("lista");
+    break
+    case "reprovada":
+        console.log("reprovada");
+    break
+
+    default:
+        console.log("não se aplica");
+}
