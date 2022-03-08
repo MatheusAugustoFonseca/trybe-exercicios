ffaf7641b7205daeb16f27d2c57f64ab42a672c6
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-03-08 16:27:01 -0300

    inicio array/for

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio2.js
@@ -0,0 +1,28 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 
+// 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+//exercicio 2
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+//exercicio 3
+// let menu = ['Home', 'Serviços', 'Portfólio', 
+// 'Links'];
+
+// menu.push("Contato");
+// console.log(menu);
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//  for(i = 0; i < groceryList.length; i += 1){
+//      console.log(groceryList[i]);
+//  }
+
+//Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let impressao of names){
+    console.log(impressao);
+}
