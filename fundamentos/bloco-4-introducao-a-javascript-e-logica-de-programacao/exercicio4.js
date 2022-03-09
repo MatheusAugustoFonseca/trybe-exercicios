8748be872bbca8e914915278a08ea4ac10c99c4d
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-03-08 21:03:22 -0300

    exercicios 1 a 9, bloco-4-dia-2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio4.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100,
+    2, 35, 27];
+let result = 0;
+    for(let i = 0; i < numbers.length; i += 1){
+    result += numbers[i];
+    }
+let avarege = (result/numbers.length);
+    console.log(avarege);
+    if (avarege > 20){
+        console.log("valor maior que 20");
+    } else {
+        console.log("Valor menor ou igual a 20")
+    }
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios9.js
@@ -0,0 +1,10 @@
+var resultado = [];
+for (let i = 0; i <= 25; i += 1) {
+    resultado.push(i);
+}
+for (let i2 = 0; i2 < resultado.length; i2 += 1) {
+    console.log(resultado[i2]/2);
+}
+
+// let divisao = resultado / 2;
+// console.log(divisao);
