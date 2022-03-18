f268b36410eb7973f752298844cb9ca174254790
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-03-18 18:47:32 -0300

    exercicios 8 e 9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8.js
@@ -0,0 +1,13 @@
+// let result = 0;
+// for(let i = 0; i <= 25; i += 1){
+//     result += i;
+// }
+// console.log(result);
+
+let numbers = [];
+
+for (let i = 1; i <= 25; i += 1) {
+  numbers.push(i);
+}
+
+console.log(numbers);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio9.js
@@ -0,0 +1,9 @@
+let numbers = [];
+
+for (let i = 1; i <= 25; i += 1) {
+  numbers.push(i);
+}
+
+for (let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index] / 2);
+}
