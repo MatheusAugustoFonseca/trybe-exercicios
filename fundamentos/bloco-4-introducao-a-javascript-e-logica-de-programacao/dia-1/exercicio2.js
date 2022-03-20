aedd1ff5159d18f7af87c183939e45e4ab17eb9e
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-03-20 19:46:56 -0300

    testes na 4.4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/exercicio2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/exercicio2.js
@@ -5,3 +5,18 @@ if (a > b){
 } else {
     console.log(b);
 }
+
+// using function
+function biggestBetweenTwo(a, b){
+    if(a > b){
+        return a + " é maior que " + b;
+    } else if (a < b) {
+        return b + " é maior que " + a;
+    } else {
+        return "Os números são iguais";
+    }
+}
+
+console.log(biggestBetweenTwo(2, 2));
+console.log(biggestBetweenTwo(2, 90));
+console.log(biggestBetweenTwo(77, 2));
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao2.js
@@ -0,0 +1,26 @@
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for(let salute in names){
+//       console.log("Olá " + names[salute]);
+//   }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+
+//   for(let full in car){
+//       console.log(full, car[full]);
+//   }
+// Com função
+function soma(a, b) {
+    return a + b;
+ 
+  
+soma(5, 2);
+}
