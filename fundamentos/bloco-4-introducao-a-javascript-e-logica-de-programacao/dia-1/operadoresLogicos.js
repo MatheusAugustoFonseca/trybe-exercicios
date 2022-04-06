e2373db5ba4d9b0103f0b1f18f7d41f6cfcf9bc7
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-04-06 16:14:16 -0300

    Revisao exercicios JS bloco4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/operadoresLogicos.js
@@ -0,0 +1,6 @@
+let weekDay = "sabado";
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe =D")
+} else {
+  console.log("FINALMENTE, DESCANSO MERECIDO");
+}
