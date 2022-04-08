e960283a323d87b270fb92148096ac3292cafc2d
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-04-08 12:51:56 -0300

    exercicios de fixacao 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-conteudo.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-conteudo.js
@@ -2,9 +2,11 @@
 // let menuServices = menu.indexOf("Portfólio") ;
 // menu.push("Contato");
 // console.log(menu);
+// console.log(menuServices);
 
 let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for( let listNames of names){
     console.log(listNames);
-    
-}
+    }
+
+for(let i = 0; i < names)
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/forExercicioFixacao.js
@@ -0,0 +1,14 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for(let i = 0; i < groceryList.length; i += 1){
+//   console.log(groceryList[i]);
+// }
+// console.log(groceryList);
+
+for(let i of groceryList){
+  console.log(i);
+}
+//FOR IN
+console.log("***********************");
+for(let i2 in groceryList){
+  console.log(i2);
+}
