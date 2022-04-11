5546310daf5857992172399b83a8bed186f7e22c
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-04-11 11:35:47 -0300

    monitoria 11/04/22

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -6,12 +6,13 @@
 let word = "Matheus";
 let contrario = "";
 
-// for (let i = (word.length - 1); i < 0 ; i-=1){
-//   contrario += word[i];
-//  }
-// console.log(contrario);
+for (let i = (word.length - 1); i >= 0 ; i-=1){
+  contrario += word[i];
+  console.log(contrario);
+ }
 
-for(let i = 0; i < word.length; i += 1){
-    contrario += word[word.length - (1 + i)]
-}
-console.log(contrario);
+
+// for(let i = 0; i < word.length; i += 1){
+//     contrario += word[word.length - (1 + i)]
+// }
+// console.log(contrario);
