7d7668a24392ae8352ee2487733cd3ee5c2f93fe
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-04-25 18:59:46 -0300

    working on day 5.3

--- fundamentos/bloco-5/dia-3-javascript-eventos/main.js
+++ fundamentos/bloco-5/dia-3-javascript-eventos/main.js
@@ -25,13 +25,11 @@ thirdLi.addEventListener('click', addingTechToLi);
 input.addEventListener('input', function(event) {
   const techElement = document.querySelector('.tech');
   techElement.innerText = event.target.value;
-  
-});
-
-
+  });
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
