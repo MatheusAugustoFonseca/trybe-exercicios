0df12c1ca782a6835be8a9c75fe040ee204afcb9
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-04-26 20:19:36 -0300

    trabalhando no js

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,6 +1,19 @@
-submit.preventDefault()
+function submit(event) {
+  event.preventDefault()
+}
+
+window.onload = function() {
+  const submitBtn = document.querySelector('#submit-btn');
+   submitBtn.addEventListener('click', submit)
+};
+
 
 function resetButton(){
+  const formElements = document.querySelectorAll('input');
+  const textArea =document.querySelector('textarea');
+  for (let index of formElements) {
+    const userInput = formElements
+  }
   
 }
 addEventListener(click, resetButton);
