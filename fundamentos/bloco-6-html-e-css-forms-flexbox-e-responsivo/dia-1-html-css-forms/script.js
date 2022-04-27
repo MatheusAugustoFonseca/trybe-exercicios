fc77a0c1051a628177847a18f4b885f476b99343
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-04-27 10:31:21 -0300

    add e configurando os botoes

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -10,10 +10,17 @@ window.onload = function() {
 
 function resetButton(){
   const formElements = document.querySelectorAll('input');
-  const textArea =document.querySelector('textarea');
-  for (let index of formElements) {
-    const userInput = formElements
+  const textArea = document.querySelector('textarea');
+  for (let index = 0; index < formElements.length; index += 1){
+    const userInput = formElements[index];
+    userInput.value = '';
+    userInput.checked = false;
   }
-  
+  textArea = '';
 }
-addEventListener(click, resetButton);
+window.onload = function() {
+  const resetBtn = document.querySelector('#reset');
+  resetBtn.addEventListener(click, resetButton);
+  // const submitBtn = document.querySelector('#submit-btn');
+  //  submitBtn.addEventListener('click', submit);
+} ;
