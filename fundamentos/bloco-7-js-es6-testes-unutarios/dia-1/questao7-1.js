18991e0d096727ff4abc39d6de4cb3747555dfc1
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-05-09 20:18:41 -0300

    estudos dia 09-05-22

---
+++ fundamentos/bloco-7-js-es6-testes-unutarios/dia-1/questao7-1.js
@@ -0,0 +1,90 @@
+// const sum = (value1, value2) => value1 + value2;
+
+// console.log(sum(2, '3'));
+// const sum = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     return `'Os valores devem ser numéricos'`;
+//   }
+//   return value1 + value2;
+// };
+
+// console.log(sum(2,'3'));
+
+// const sum = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+//   return value1 + value2;
+// };
+
+// console.log(sum(2, '5'));
+// const verifyIsNumber = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+// };
+
+// const sum = (value1, value2) => {
+//   try {
+//     verifyIsNumber(value1, value2);
+//     return value1 + value2;
+//   } catch (error) {
+//     throw error.message;
+//   }
+// };
+
+// console.log(sum(2, '3'));
+
+// const customer1 = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// console.log(customer1);
+
+// customer1.lastName = 'Faria';
+// console.log(customer1);
+
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// let newKey = 'lastName';
+// const lastName = 'Ferreira';
+// customer[newKey] = lastName;
+// newKey = 'fullName';
+// const fullName = `${customer.firstName} ${customer.lastName}`;
+// customer[newKey] = fullName;
+// console.log(customer);
+
+// const student = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkill: 'Ótimo',
+// };
+// const listSkillsValuesWithValues = (student) => Object.values(student);
+// // console.log(listSkillsValuesWithValues(student));
+// console.log(Object.entries(student));
+
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
