4516d8c7fa2dd06ff5241f14d9c4ee3d70b99e52
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-05-27 16:17:09 -0300

    exercise 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3/exercise1.js
@@ -1,60 +1,60 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -64,15 +64,24 @@ const books = [
 // Adicione o código do exercício aqui:
 //🚀 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  "As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin",
+  "O Senhor dos Anéis - Fantasia - J. R. R. Tolkien",
+  "Fundação - Ficção Científica - Isaac Asimov",
+  "Duna - Ficção Científica - Frank Herbert",
+  "A Coisa - Terror - Stephen King",
+  "O Chamado de Cthulhu - Terror - H. P. Lovecraft",
 ];
 
 function formatedBookNames() {
   // escreva seu código aqui
-  books.map((report)=>`${report.name} - ${report.genre} - ${report.author}`);
+  return books.map((report) => `${report.name} - ${report.genre} - ${report.author}`);
 }
+//🚀 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+function nameAndAge() {
+  // escreva seu código aqui
+  return books.map((book)=> ({
+    author: book.author.name,
+    age: book.releaseYear - book.author.birthYear
+  })).sort((authorA, authorB)=> authorA.age - authorB.age);
+}
+console.log(nameAndAge());
