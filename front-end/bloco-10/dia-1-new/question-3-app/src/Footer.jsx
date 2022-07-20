a602dad2eca5a9f09a91bf3eb2170abf2f976f5b
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-07-20 19:32:31 -0300

    exercise 3

---
+++ front-end/bloco-10/dia-1-new/question-3-app/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <div>
+        <h3 className='footer'>E isso é só o começo...</h3>
+      </div>
+    )
+  }
+}
+
+export default Footer;
