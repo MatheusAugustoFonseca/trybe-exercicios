8e149f6885e6e7023577d8f9321820c093421142
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-07-21 20:13:30 -0300

    exercicio de fixacao 1

---
+++ front-end/bloco-10/dia-2-new/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,17 @@
+// import cat from '../'
+import Image from './Image';
+
+function App() {
+  return (
+    <div>
+      <Image
+      source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+      alternativeText="Cute cat" />
+    </div>
+  );
+}
+
+export default App;
+//https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg
+
+// Cute cat staring
