98254bf63de3d286c7d1eed50cd9cfa8330cf161
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-07-25 18:25:57 -0300

    exercicio pokedex

---
+++ front-end/bloco-10/dia-2-new/my-pokedex/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Pokedex from './Pokedex';
+import data from './data';
+
+function App() {
+  return (
+    <div>
+      <h1>Pokedex</h1>
+      <Pokedex pokemons={data}/>
+    </div>
+  );
+}
+
+export default App;
