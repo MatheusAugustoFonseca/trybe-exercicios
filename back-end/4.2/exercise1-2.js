1960c20bf3e28bb5a29904091ecdb3908e067951
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-11-17 15:26:10 -0300

    day 2 - exercises

---
+++ back-end/4.2/exercise1-2.js
@@ -0,0 +1,8 @@
+// 400
+// The HyperText Transfer Protocol (HTTP) 400 Bad Request response status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error (for example, malformed request syntax, invalid request message framing, or deceptive request routing)
+
+//422
+// The HyperText Transfer Protocol (HTTP) 422 Unprocessable Entity response status code indicates that the server understands the content type of the request entity, and the syntax of the request entity is correct, but it was unable to process the contained instructions.
+
+//401
+// The HyperText Transfer Protocol (HTTP) 401 Unauthorized response status code indicates that the client request has not been completed because it lacks valid authentication credentials for the requested resource.
