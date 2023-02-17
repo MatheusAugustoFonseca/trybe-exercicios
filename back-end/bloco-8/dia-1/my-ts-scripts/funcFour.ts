4b1c95a64f99c0e17bf1cbd59edd3cfba8c82921
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-02-17 19:35:46 -0300

    exercises

---
+++ back-end/bloco-8/dia-1/my-ts-scripts/funcFour.ts
@@ -0,0 +1,16 @@
+// Essa função é responsável por receber um objeto e formar uma frase utilizando as chaves do mesmo.
+const peopleInput = {
+  name: 'Rui',
+  age: 23,
+};
+
+// const User2 = {
+//   name: string,
+//   age: number,
+// }
+
+const createSimpleSentence = (param: {name: string, age: number }) => {
+  return `Olá, meu nome é ${param.name} e tenho ${param.age} anos.`;
+}
+
+console.log(createSimpleSentence(peopleInput));
