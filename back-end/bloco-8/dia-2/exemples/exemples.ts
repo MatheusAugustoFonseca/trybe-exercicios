f10ffd10b131d42ea597bddad03f1a2618116b68
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-02-19 20:14:24 -0300

    Exemples on TS classes

---
+++ back-end/bloco-8/dia-2/exemples/exemples.ts
@@ -0,0 +1,31 @@
+// Crie uma classe cujo objeto represente um Cachorro.
+
+class Dog {
+  name: string;
+  birthDate: Date;
+  age: number;
+  breed: string;
+
+    constructor(name: string, birthDate: Date, age: number, breed: string) {
+      this.name = name;
+      this.birthDate = birthDate;
+      this.age = age;
+      this.breed = breed;
+    }
+
+    bark(): void {
+      console.log(`${this.name} said: AuAuAu`);
+    }
+
+    play(): void {
+      console.log(`${this.name} is playing`);
+    }
+
+    fetch(): void {
+      console.log(`${this.name} is fetching`);
+    }
+}
+
+const dog1 = new Dog('Rex', new Date('2020-09-17'), 2, 'poodle');
+console.log(dog1);
+dog1.bark();
