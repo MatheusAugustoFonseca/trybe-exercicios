e539db648c5dd5275cae720c5296e12662c11f4a
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-03-10 10:32:04 -0300

    criando classe person com validacoes

---
+++ back-end/bloco-9/dia-2/index.ts
@@ -0,0 +1,7 @@
+import Person from "./Person";
+
+const geralda = new Person('Geralda Sousa', new Date('1970/05/30'));
+const fernanda = new Person('Fernada Sousa', new Date('19/05/05'));
+
+console.log(geralda);
+console.log(fernanda);
